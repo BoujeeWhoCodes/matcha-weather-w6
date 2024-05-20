60217d9042272e734a2ee49b7a813c3db98430ad
BoujeeWhoCodes <imellettis28@gmail.com>
2024-05-20 17:19:29 +0800

    Update files

---
+++ src/CurrentTemp.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+export default function CurrentTemp(props) {
+	return (
+		<div>
+			<div className='current-temp-container'>
+				<span className='current-temp-value'>{Math.round(props.celsius)}</span>
+				<span className='current-temp-unit'>°C</span>
+			</div>
+		</div>
+	);
+}
