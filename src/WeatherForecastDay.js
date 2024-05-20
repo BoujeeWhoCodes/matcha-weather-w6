e5114d8162c8a5e855750e691fc230834580f79e
BoujeeWhoCodes <imellettis28@gmail.com>
2024-05-20 17:43:30 +0800

    Create WeatherForecastDay.js

---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,51 @@
+/** @format */
+
+import React from 'react';
+import WeatherIcons from './WeatherIcons';
+
+export default function WeatherForecastDay(props) {
+	function maxTemperature() {
+		let temperature = Math.round(props.data.temp.max);
+		return `${temperature}°`;
+	}
+
+	function minTemperature() {
+		let temperature = Math.round(props.data.temp.min);
+		return `${temperature}°`;
+	}
+
+	function day() {
+		let date = new Date(props.data.dt * 1000);
+		let day = date.getDay();
+
+		let days = [
+			'Sunday',
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+			'Saturday',
+		];
+
+		return days[day];
+	}
+
+	return (
+		<div className='forecast-day'>
+			<div className='forecast-icon'>
+				<WeatherIcons
+					code={props.data.weather[0].icon}
+					size={36}
+					color='#8ca1b7'
+				/>
+			</div>
+			<div className='forecast-date'>{day()}</div>
+			<div className='forecast-temperature'>
+				<span className='forecast-temps-max'>{maxTemperature()}</span> /
+				<span className='forecast-temps-min'>{minTemperature()}</span>{' '}
+				<span className='forecast-unit'>ºC</span>
+			</div>
+		</div>
+	);
+}
