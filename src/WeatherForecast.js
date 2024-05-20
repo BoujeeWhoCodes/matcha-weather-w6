e380fa5a7098d9c308d09a4c205b70e25e6a67be
BoujeeWhoCodes <imellettis28@gmail.com>
2024-05-20 17:42:56 +0800

    Create WeatherForecast.js

---
+++ src/WeatherForecast.js
@@ -0,0 +1,48 @@
+/** @format */
+
+import React, { useState, useEffect } from 'react';
+import './WeatherForecast.css';
+import axios from 'axios';
+import WeatherForecastDay from './WeatherForecastDay';
+
+export default function WeatherForecast(props) {
+	let [loaded, setLoaded] = useState(false);
+	let [forecast, setForecast] = useState(null);
+
+	useEffect(() => {
+		setLoaded(false);
+	}, [props.coordinates]);
+
+	function handleResponse(response) {
+		setForecast(response.data.daily);
+		setLoaded(true);
+	}
+
+	if (loaded) {
+		return (
+			<div className='weather-forecast-container'>
+				{forecast.map(function (dailyForecast, index) {
+					if (index < 6) {
+						return (
+							<div key={index}>
+								<WeatherForecastDay data={dailyForecast} />
+							</div>
+						);
+					} else {
+						return null;
+					}
+				})}
+			</div>
+		);
+	} else {
+		const apiKey = '1fd8093fa5ff12d796d7de756cc9d6b9';
+		let latitude = props.coordinates.lat;
+		let longitude = props.coordinates.lon;
+
+		let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+		axios.get(apiUrl).then(handleResponse);
+
+		return null;
+	}
+}
